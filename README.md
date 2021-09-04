74f17ea9523656f79e7139a392c0a36392d74a42
dslguo <sguo0011@student.monash.edu>
2021-09-04 21:37:12 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Super Discord Bot
+ A side project where I explore Java, Spring and Maven to create a discord bot with a wide range of functionalities.
---
+++ src/main/java/com/example/demo/SuperDiscordBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperDiscordBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperDiscordBotApplication.class, args);
+	}
+
+}
