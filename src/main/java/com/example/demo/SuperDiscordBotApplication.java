7adcb9aace6106c9e5bda13c052a1abd13a796d3
dslguo <sguo0011@student.monash.edu>
2021-09-04 22:42:26 +1000

    feat: Bringing SuperDiscordBot to life :)
    adding its token to our configuration.
    It now exists in my discord server and whenever I type ".ping", the bot replies with Pong!

--- src/main/java/com/example/demo/SuperDiscordBotApplication.java
+++ src/main/java/com/example/demo/SuperDiscordBotApplication.java
@@ -1,13 +1,37 @@
 package com.example.demo;
 
+import org.javacord.api.DiscordApi;
+import org.javacord.api.DiscordApiBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class SuperDiscordBotApplication {
+
+	@Autowired
+	private Environment env;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SuperDiscordBotApplication.class, args);
 	}
 
+	@Bean
+	@ConfigurationProperties(value = "discord-api")
+	public DiscordApi discordApi() {
+		String token = env.getProperty("TOKEN");
+		DiscordApi api = new DiscordApiBuilder().setToken(token)
+				.setAllNonPrivilegedIntents()
+				.login()
+				.join();
+		api.addMessageCreateListener(event -> {
+			if(event.getMessageContent().equals(".ping")) {
+				event.getChannel().sendMessage("Pong!");
+			}
+		});
+		return api;
+	}
 }
